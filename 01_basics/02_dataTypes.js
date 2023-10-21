c3600e533c57eb2a96a4578d93c09261a67ccea4
raheemmern <148579450+raheemmern@users.noreply.github.com>
2023-10-21 07:34:38 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"Use Strict"; //Treat All Js Code As Newer Version Js 
+// alert(3 + 3); We Are Using Node Js Not Browser There Is A Different Syntax For Alert In Node Js
+
+console.log(3 
+    + 
+    3) //Code Readibility Should Be High And Priority To Make It Future Proof And Show Your Experience As A Developer
+
+console.log("Raheem");
+
+let name = "Abdul Raheem"
+let age = 21
+let isLoggedIn = true
+let state;
+
+// Types Of Data
+
+// number => maximum value you can store 2 the power of 53
+// bigint => Used for bigger number of data billions trillions
+// string => "" you can use either sigle quotes '' or you can use double quotes "" mostly prefer double quotes ""
+// boolean => true/false boolean used to check if a condition is true or false
+// null => Standalone value
+// undefined => 
+// symbol => mostly used for uniqueness in react
+
+// object
+
+console.log(typeof null); // output object
+console.log(typeof undefined); // output undefined
