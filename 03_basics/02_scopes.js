4dc737d1a1b2b10549a8a15839ad9ae032a64079
raheemmern <148579450+raheemmern@users.noreply.github.com>
2024-04-24 12:07:43 +0000

    Scopes And Mini Hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -14,7 +14,69 @@ if (true) {
 
 // global scope in browser console is different then the global scope in the code
 
-console.log(a);
-console.log(b);
-console.log(c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
+// example of nested functions and how does those works and how scopes inside nested functions work
+
+function one (){
+   const name = "Cop"
+
+    function two (){
+      const  username = "none"
+        console.log(name); // so basically you can access the value of outer function from inner function but you cannot access the value from inner function to outer function 
+    }
+
+    // two()
+}
+// one()
+
+if (true) {
+    const username = "Designs"
+    if (username === "Designs")  {
+        const wesbite = "Youtube"
+        console.log(`Username Is ${username} Website Is ${wesbite}`);
+    }
+    // console.log(wesbite); // there is going to be a error because its out of its scope 
+}
+// console.log(username); // same here there is going to be a error
+
+
+
+// +++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++++++//
+
+// function syntax 1 
+
+function addone (num){
+   return num + 1
+}
+console.log(addone(5)) // in this syntax you can log it in before the function when you are not storing it in a variable
+
+// function syntax 2 
+
+const addtwo = function(num2){
+    return num2 + 2
+}
+
+addtwo(5)  
+console.log(addtwo); // if you try to log it before the function then its not gonna work when you are storing it in a variable in this syntax 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
