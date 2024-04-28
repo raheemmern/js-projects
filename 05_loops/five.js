ba992f5bebafec7dd7244e91adb0d0855d8a9416
raheemmern <148579450+raheemmern@users.noreply.github.com>
2024-04-28 13:44:31 +0000

    For Each Loop

---
+++ 05_loops/five.js
@@ -0,0 +1,18 @@
+// for each loop
+
+
+const coding = ["python" , "cpp" , "java" , "ruby" , "js"]
+
+// arrays agr ay gy to loop to lgana he pryga values to chahy hogi na uski
+
+// you can access different type of prototypes by writing the variable and then placing . afterwards
+// like this way coding. and there will be lot of suggestions
+
+coding.
+
+
+
+
+
+
+
