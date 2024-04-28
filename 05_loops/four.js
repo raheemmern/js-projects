9ae47c12231c281c1438382778b506fb7f368cc2
raheemmern <148579450+raheemmern@users.noreply.github.com>
2024-04-28 12:19:52 +0000

    For In Loop

---
+++ 05_loops/four.js
@@ -0,0 +1,33 @@
+// for in loop used for objects or on objects
+
+
+const myObject = {
+    js : "Javascript",
+    py : "Python",
+    cpp : "C++",
+    swift : "Swift By Apple"
+}
+
+for (const key in myObject) {
+   console.log(`${key} Is Shortcut For ${myObject[key]}`); // 
+}
+
+// arrays also have keys and it starts from 0 
+// for in loop just picks the keys from arrays not the direct values like for of loop does
+
+const programming = [ "Java" , "C++" , "Python"]
+
+for (const key in programming) {
+    console.log(key); // by using this method you are going to get keys in this array the keys are 0 , 1 , 2 as there are 3 values
+}
+
+// if you want to access the values inside the programming array you need to use the syntax like this
+
+for (const key in programming) {
+    console.log(programming[key]); // by using this method you will get the values of the keys values
+}
+
+
+
+
+
