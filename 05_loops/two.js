241ab74c2d078bf541280a4d5a34c97fab635d4b
raheemmern <148579450+raheemmern@users.noreply.github.com>
2024-04-28 11:28:06 +0000

    While And Do While Loops In Javascript

---
+++ 05_loops/two.js
@@ -0,0 +1,30 @@
+let index = 0
+
+while (index <= 10) {
+    console.log(`Value Of Index Right Now Is ${index}`);
+    index = index + 2
+}
+
+let myArray = ["Flash", "Batman", "Superman"]
+
+let arr = 0
+
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);  // so the do while loop works like this first it do the work and then check the condition unlike while loop or for loop
+
+// most of the values you are gonna get from the database is in the shape of arrays and applying loops on arrays is the most common thing you are going to do
+
+
+
+
+
+
